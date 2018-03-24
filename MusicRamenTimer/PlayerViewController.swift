1d69510117b906b2bbba7e0265591e307421ebcf
Syunpei Nagata <n1510124@edu.cc.uec.ac.jp>
2018-03-24 15:42:38 +0900

    Initial Commit

---
+++ MusicRamenTimer/PlayerViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PlayerViewController.swift
+//  MusicRamenTimer
+//
+//  Created by 永田駿平 on 2018/01/29.
+//  Copyright © 2018年 SyunpeiNagata. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import MediaPlayer
+
+class PlayerViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
